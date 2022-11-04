d8595170d941ef01301c5f76cb2536640daf2628
Willian Mano <willianmanoaraujo@gmail.com>
2022-11-04 15:05:21 -0300

    Release for Moodle 4.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_youtubewpt';
-$plugin->release = '0.1.0';
-$plugin->version = 2022010400;
-$plugin->requires = 2020061500;
+$plugin->release = '1.0.0';
+$plugin->version = 2022110400;
+$plugin->requires = 2022041200;
 $plugin->maturity = MATURITY_BETA;
