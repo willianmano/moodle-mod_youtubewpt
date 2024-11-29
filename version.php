0ea9f54547a317f801e34319f4d0ac0b51bee6c1
Willian Mano <willianmanoaraujo@gmail.com>
2024-11-29 17:15:22 -0300

    Aumenta versao

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_youtubewpt';
-$plugin->release = '1.0.0';
-$plugin->version = 2022110400;
+$plugin->release = '1.1.0';
+$plugin->version = 2024112900;
 $plugin->requires = 2023100900;
 $plugin->maturity = MATURITY_STABLE;
